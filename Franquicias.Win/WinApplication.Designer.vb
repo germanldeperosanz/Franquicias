b1d132f1ef169db6ebeb9f28a7ef8b594bccbe2a
German <gldeperosanz@gmail.com>
2020-05-01 18:20:51 -0300

    XAF Wizard

---
+++ Franquicias.Win/WinApplication.Designer.vb
@@ -0,0 +1,147 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+
+Partial Public Class FranquiciasWindowsFormsApplication
+	''' <summary> 
+	''' Required designer variable.
+	''' </summary>
+	Private components As System.ComponentModel.IContainer = Nothing
+
+	''' <summary> 
+	''' Clean up any resources being used.
+	''' </summary>
+	''' <param name="disposing">true if managed resources should be disposed otherwise, false.</param>
+	Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+		If disposing AndAlso (Not components Is Nothing) Then
+			components.Dispose()
+		End If
+		MyBase.Dispose(disposing)
+	End Sub
+
+#Region "Component Designer generated code"
+
+	''' <summary> 
+	''' Required method for Designer support - do not modify 
+	''' the contents of this method with the code editor.
+	''' </summary>
+	Private Sub InitializeComponent()
+		Me.module1 = New DevExpress.ExpressApp.SystemModule.SystemModule()
+        Me.module2 = New DevExpress.ExpressApp.Win.SystemModule.SystemWindowsFormsModule()
+		Me.module3 = New Global.Franquicias.Module.FranquiciasModule()
+		Me.module4 = New Global.Franquicias.Module.Win.FranquiciasWindowsFormsModule()
+        Me.securityModule1 = New DevExpress.ExpressApp.Security.SecurityModule()
+        Me.securityStrategyComplex1 = New DevExpress.ExpressApp.Security.SecurityStrategyComplex()
+        Me.securityStrategyComplex1.SupportNavigationPermissionsForTypes = False
+        Me.objectsModule = New DevExpress.ExpressApp.Objects.BusinessClassLibraryCustomizationModule()
+        Me.chartModule = New DevExpress.ExpressApp.Chart.ChartModule()
+        Me.chartWindowsFormsModule = New DevExpress.ExpressApp.Chart.Win.ChartWindowsFormsModule()
+        Me.cloneObjectModule = New DevExpress.ExpressApp.CloneObject.CloneObjectModule()
+        Me.conditionalAppearanceModule = New DevExpress.ExpressApp.ConditionalAppearance.ConditionalAppearanceModule()
+        Me.dashboardsModule = New DevExpress.ExpressApp.Dashboards.DashboardsModule()
+        Me.dashboardsWindowsFormsModule = New DevExpress.ExpressApp.Dashboards.Win.DashboardsWindowsFormsModule()
+        Me.fileAttachmentsWindowsFormsModule = New DevExpress.ExpressApp.FileAttachments.Win.FileAttachmentsWindowsFormsModule()
+        Me.pivotChartModuleBase = New DevExpress.ExpressApp.PivotChart.PivotChartModuleBase()
+        Me.pivotChartWindowsFormsModule = New DevExpress.ExpressApp.PivotChart.Win.PivotChartWindowsFormsModule()
+        Me.pivotGridModule = New DevExpress.ExpressApp.PivotGrid.PivotGridModule()
+        Me.pivotGridWindowsFormsModule = New DevExpress.ExpressApp.PivotGrid.Win.PivotGridWindowsFormsModule()
+        Me.reportsModuleV2 = New DevExpress.ExpressApp.ReportsV2.ReportsModuleV2()
+        Me.reportsWindowsFormsModuleV2 = New DevExpress.ExpressApp.ReportsV2.Win.ReportsWindowsFormsModuleV2()
+        Me.stateMachineModule = New DevExpress.ExpressApp.StateMachine.StateMachineModule()
+        Me.validationModule = New DevExpress.ExpressApp.Validation.ValidationModule()
+        Me.validationWindowsFormsModule = New DevExpress.ExpressApp.Validation.Win.ValidationWindowsFormsModule()
+        Me.viewVariantsModule = New DevExpress.ExpressApp.ViewVariantsModule.ViewVariantsModule()
+        Me.authenticationStandard1 = New DevExpress.ExpressApp.Security.AuthenticationStandard()
+		CType(Me, System.ComponentModel.ISupportInitialize).BeginInit()
+        ' 
+        ' securityStrategyComplex1
+        ' 
+        Me.securityStrategyComplex1.Authentication = Me.authenticationStandard1
+        Me.securityStrategyComplex1.RoleType = GetType(DevExpress.Persistent.BaseImpl.PermissionPolicy.PermissionPolicyRole)
+        Me.securityStrategyComplex1.UserType = GetType(DevExpress.Persistent.BaseImpl.PermissionPolicy.PermissionPolicyUser)
+        ' 
+        ' securityModule1
+        ' 
+        Me.securityModule1.UserType = GetType(DevExpress.Persistent.BaseImpl.PermissionPolicy.PermissionPolicyUser)
+        ' 
+        ' authenticationStandard1
+        ' 
+        Me.authenticationStandard1.LogonParametersType = GetType(DevExpress.ExpressApp.Security.AuthenticationStandardLogonParameters)
+        '
+        ' dashboardsModule
+        '
+        Me.dashboardsModule.DashboardDataType = GetType(DevExpress.Persistent.BaseImpl.DashboardData)
+        Me.dashboardsWindowsFormsModule.DesignerFormStyle = DevExpress.XtraBars.Ribbon.RibbonFormStyle.Ribbon
+        '
+        ' reportsModuleV2
+        '
+        Me.reportsModuleV2.EnableInplaceReports = True
+        Me.reportsModuleV2.ReportDataType = GetType(DevExpress.Persistent.BaseImpl.ReportDataV2)
+        Me.reportsModuleV2.ShowAdditionalNavigation = False
+        Me.reportsModuleV2.ReportStoreMode = DevExpress.ExpressApp.ReportsV2.ReportStoreModes.XML
+        '
+        ' stateMachineModule
+        '
+        Me.stateMachineModule.StateMachineStorageType = GetType(DevExpress.ExpressApp.StateMachine.Xpo.XpoStateMachine)
+        ' 
+		' FranquiciasWindowsFormsApplication
+		' 
+        Me.ApplicationName = "Franquicias"
+        Me.CheckCompatibilityType = DevExpress.ExpressApp.CheckCompatibilityType.DatabaseSchema
+        Me.Modules.Add(Me.module1)
+		Me.Modules.Add(Me.module2)
+		Me.Modules.Add(Me.module3)
+		Me.Modules.Add(Me.module4)
+        Me.Modules.Add(Me.securityModule1)
+        Me.Security = Me.securityStrategyComplex1
+        Me.Modules.Add(Me.objectsModule)
+        Me.Modules.Add(Me.chartModule)
+        Me.Modules.Add(Me.chartWindowsFormsModule)
+        Me.Modules.Add(Me.cloneObjectModule)
+        Me.Modules.Add(Me.conditionalAppearanceModule)
+        Me.Modules.Add(Me.dashboardsModule)
+        Me.Modules.Add(Me.dashboardsWindowsFormsModule)
+        Me.Modules.Add(Me.fileAttachmentsWindowsFormsModule)
+        Me.Modules.Add(Me.pivotChartModuleBase)
+        Me.Modules.Add(Me.pivotChartWindowsFormsModule)
+        Me.Modules.Add(Me.pivotGridModule)
+        Me.Modules.Add(Me.pivotGridWindowsFormsModule)
+        Me.Modules.Add(Me.reportsModuleV2)
+        Me.Modules.Add(Me.reportsWindowsFormsModuleV2)
+        Me.Modules.Add(Me.stateMachineModule)
+        Me.Modules.Add(Me.validationModule)
+        Me.Modules.Add(Me.validationWindowsFormsModule)
+        Me.Modules.Add(Me.viewVariantsModule)
+        Me.UseOldTemplates = False
+
+		CType(Me, System.ComponentModel.ISupportInitialize).EndInit()
+
+	End Sub
+
+#End Region
+
+	Private module1 As DevExpress.ExpressApp.SystemModule.SystemModule
+    Private module2 As DevExpress.ExpressApp.Win.SystemModule.SystemWindowsFormsModule
+	Private module3 As Global.Franquicias.Module.FranquiciasModule
+    Private module4 As Global.Franquicias.Module.Win.FranquiciasWindowsFormsModule
+    Private securityModule1 As DevExpress.ExpressApp.Security.SecurityModule
+    Private securityStrategyComplex1 As DevExpress.ExpressApp.Security.SecurityStrategyComplex
+    Private authenticationStandard1 As DevExpress.ExpressApp.Security.AuthenticationStandard
+    Private objectsModule As DevExpress.ExpressApp.Objects.BusinessClassLibraryCustomizationModule
+    Private chartModule As DevExpress.ExpressApp.Chart.ChartModule
+    Private chartWindowsFormsModule As DevExpress.ExpressApp.Chart.Win.ChartWindowsFormsModule
+    Private cloneObjectModule As DevExpress.ExpressApp.CloneObject.CloneObjectModule
+    Private conditionalAppearanceModule As DevExpress.ExpressApp.ConditionalAppearance.ConditionalAppearanceModule
+    Private dashboardsModule As DevExpress.ExpressApp.Dashboards.DashboardsModule
+    Private dashboardsWindowsFormsModule As DevExpress.ExpressApp.Dashboards.Win.DashboardsWindowsFormsModule
+    Private fileAttachmentsWindowsFormsModule As DevExpress.ExpressApp.FileAttachments.Win.FileAttachmentsWindowsFormsModule
+    Private pivotChartModuleBase As DevExpress.ExpressApp.PivotChart.PivotChartModuleBase
+    Private pivotChartWindowsFormsModule As DevExpress.ExpressApp.PivotChart.Win.PivotChartWindowsFormsModule
+    Private pivotGridModule As DevExpress.ExpressApp.PivotGrid.PivotGridModule
+    Private pivotGridWindowsFormsModule As DevExpress.ExpressApp.PivotGrid.Win.PivotGridWindowsFormsModule
+    Private reportsModuleV2 As DevExpress.ExpressApp.ReportsV2.ReportsModuleV2
+    Private reportsWindowsFormsModuleV2 As DevExpress.ExpressApp.ReportsV2.Win.ReportsWindowsFormsModuleV2
+    Private stateMachineModule As DevExpress.ExpressApp.StateMachine.StateMachineModule
+    Private validationModule As DevExpress.ExpressApp.Validation.ValidationModule
+    Private validationWindowsFormsModule As DevExpress.ExpressApp.Validation.Win.ValidationWindowsFormsModule
+    Private viewVariantsModule As DevExpress.ExpressApp.ViewVariantsModule.ViewVariantsModule
+End Class
